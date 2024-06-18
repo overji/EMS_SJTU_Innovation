1f58b130e4258ae2c509353c2f4441bb2b2b7e87
Lonewiser <152686440+Lonewiser@users.noreply.github.com>
2024-06-18 17:21:20 +0800

    更新登录界面时更新了main.py但是被覆盖了这里恢复一下

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ if __name__ == '__main__':
     main.pushButton.clicked.connect(main.cutt)
     main.pushButton_2.clicked.connect(wangjimima.Open3)
     main.pushButton_3.clicked.connect(register.Open2)
-    main.pushButton_4.clicked.connect(main.change2)
-
-    main.pushButton_6.clicked.connect(main.change1)
 
     wangjimima.pushButton_2.clicked.connect(main.Open)
     wangjimima.pushButton.clicked.connect(wangjimima.ct)
@@ -23,7 +20,6 @@ if __name__ == '__main__':
     register.pushButton_2.clicked.connect(main.Open)
 
     main.show()
-    main.pushButton_4.close()
 
     rect = QDesktopWidget().availableGeometry().getRect()
     print(rect)
